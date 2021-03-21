62032671c57cae980145b2a84cea5a0a9e88f58a
Toby <rowlandsToby1@gmail.com>
2021-03-21 17:37:53 +0000

    Added no-littering and page-break-lines
    Should help keep further installs tidy and readable

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,16 +1,37 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(load "~/.emacs.d/paths.el")
+(load "~/.emacs.d/my-paths.el")
+
+;;; Use-Package config and auto-package-update
+
+;https://github.com/jwiegley/use-package
+(eval-when-compile
+  (require 'use-package))
+(require 'use-package-ensure)
+(setq use-package-always-ensure t);install not already installed packages
+
+
+(use-package auto-package-update
+  :ensure t
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
+
+;;; Files to keep further installs tidy
+
+;https://github.com/emacscollective/no-littering
+(use-package no-littering
+  :ensure t
+  :init
+  (setq no-littering-var-directory (expand-file-name "cache/data/" user-emacs-directory))
+  (setq no-littering-etc-directory (expand-file-name "cache/etc/" user-emacs-directory))
+  (setq custom-file (expand-file-name "custom.el" user-emacs-directory)))
+
+;https://github.com/purcell/page-break-lines
+(use-package page-break-lines
+  :ensure t
+  :config
+  (setq page-break-lines-mode 1))
