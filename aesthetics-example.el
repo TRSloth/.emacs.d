b12eae76aeabba08f32e58a94d82366047662035
Toby <rowlandsToby1@gmail.com>
2021-03-24 18:26:07 +0000

    Added instructions to aesthetics file
    Muted setting of a font

--- aesthetics-example.el
+++ aesthetics-example.el
@@ -1,17 +1,29 @@
-;;; Applying Themes
-;; Fonts
-;;(set-face-attribute 'default nil :font "DejaVu Sans-12")b
-(set-face-attribute 'default nil :font "dubai-12")
-;;(set-face-attribute 'default nil  :font "courier-12")
-;; Theme
+;;;; Customise this to your liking
+;;; Trying things out
+; To test a font or theme scroll to after its final bracket and enter "C-x C-e"
+
+;;; Linking to this file
+; Either change to your variables and remove "-example" from the filename or create a symlink with 
+; Windows
+; mklink aesthetics.el aesthetics-example.el
+; Linux
+; ln -s aesthetics-example.el aesthetics.el 
+; doing it this way will allow you to compare your version to changes I make using diff on Github desktop
+
+;;;; Fonts and Themes
+;;; Fonts-unset as I don't know what you've got installed
+;(set-face-attribute 'default nil :font "DejaVu Sans-12")		; Monospaced font, more readable than courier		
+;(set-face-attribute 'default nil :font "dubai-12")			; My default from https://dubaifont.com/
+;(set-face-attribute 'default nil  :font "courier-12")			; Monospaced font usuall Pre-installed
+;;; Theme installation
 (use-package modus-operandi-theme :ensure t  :defer t)
 (use-package modus-vivendi-theme :ensure t :defer t)
-;(load-theme 'modus-operandi t)
-(load-theme 'modus-vivendi t)
-;(dolist (theme custom-enabled-themes)  (disable-theme theme))
+;(load-theme 'modus-operandi t)						; light theme
+(load-theme 'modus-vivendi t)						; dark theme
+;(dolist (theme custom-enabled-themes)  (disable-theme theme))		; disable current theme
 
 
-;;; built-in look and feel options
+;;;; built-in look and feel options
 (setq-default word-wrap t)
 (setq global-visual-line-mode t)
 (setq-default line-spacing 0)
