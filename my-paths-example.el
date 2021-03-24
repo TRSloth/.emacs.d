1651e9f8b6393e571a600a756e5865c1691712f7
Toby <rowlandsToby1@gmail.com>
2021-03-24 18:10:25 +0000

    Improved comments
    Added additional configuration help and cleaned-up comments

--- my-paths-example.el
+++ my-paths-example.el
@@ -1,12 +1,12 @@
 ;;;; Link files in Windows
 (when (string-equal system-type "windows-nt")
   (setenv "HOME" "G:")						; this is where '~' refers to
-  (setq user-emacs-directory "~/.emacs.d/") 				; needs to specify full-path
-  (setq org-roam-graph-executable "~/.emacs.d/win/Graphviz/bin/dot.exe"); executable to make graphs (already downloaded)
+  (setq user-emacs-directory "~/.emacs.d/")
+  (setq org-roam-graph-executable "~/.emacs.d/win/Graphviz/bin/dot.exe")	; addition windows file for graph visualisation
   (setq TeX-macro-global						; where you've downloaded Texlive only required for Latex
    '("c:/texlive/2020/texmf-var/tex/" "c:/texlive/texmf-local/tex/" "c:/texlive/texmf-local/bibtex/bst/" "c:/texlive/2020/texmf-dist/tex/" "c:/texlive/2020/texmf-dist/bibtex/bst/"))
 ;;; Locate linux functions in windows
-  (add-to-list 'exec-path "~/.emacs.d/win") 				; for any additional files needed by windows
+  (add-to-list 'exec-path "~/.emacs.d/win") 				; additional Windows files already installed
   (setenv "PATH"
     (concat						       	; Paths for Windows version of linux executables
      "C:/msys64/mingw64/bin" ";"					; if you have it installed it is prefered over cygwin
@@ -32,4 +32,3 @@
 (setq org-roam-directory "~/entropy/")	       			; Where you keep your org-roam notes
 (setq org-brain-path "~/entropy/brain")				; Structured org-brain notes
 (setq bibtex-completion-bibliography '("~/entropy/roam.bib"))		; Your main bibleography
-
