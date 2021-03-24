a1499bfd2c67a7c487bdc27aa409386667f5b20e
Toby <rowlandsToby1@gmail.com>
2021-03-24 16:29:05 +0000

    Changed my config files to examples
    Causes default to load when install specific files haven't been added

---
+++ pastebin-example.el
@@ -0,0 +1,5 @@
+;You can assign any key you like I use keys starting with "C-c a" as only "C-c a c" is already assigned
+;C-c a c already defined as inserting acronym
+(global-set-key (kbd "C-c a a") "#+roam_alias: ")
+(global-set-key (kbd "C-c a s") "#+startup: content\n")
+(global-set-key (kbd "C-c a t") "#+roam_tags: ")
