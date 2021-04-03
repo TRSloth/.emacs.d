0208b63853d212e0f63fb867b6c487f1a198490b
Toby <rowlandsToby1@gmail.com>
2021-04-03 02:10:46 +0100

    Refactor
    Re-ordered sequence to improve readability

--- my-paths-example.el
+++ my-paths-example.el
@@ -10,15 +10,28 @@
 
 ;;;; Link files in Windows
 (when (string-equal system-type "windows-nt")
-  (setenv "HOME" "C:/Repos/IndividualProject_2020_Toby-Rowlands")	; this is where '~' refers to
-  (setq user-emacs-directory "~/.emacs.d/")
-(setq pdf-info-epdfinfo-program "~/.emacs.d/win/epdfinfo.exe")
-; (setq org-roam-graph-executable "~/.emacs.d/win/Graphviz/bin/dot.exe"); allows org-roam-graph, though functionality is eclipsed by org-roam-server-mode so I've removed it
-  (setq TeX-macro-global						; where you've downloaded Texlive only required for Latex
+  (setenv "HOME" "C:/repo/IndividualProject_2020_Toby-Rowlands"))	; MUST, IF using Windows,
+
+(when (string-equal system-type "gnu/linux")
+  (setenv "HOME" "/home/chaos")			       			; MUST, change if using LINUX to the parent dir of this directory
+  (setq TeX-macro-global "/usr/bin/"))					; MAY, set TeX install location
+
+
+;;; Directory check
+; If given user directory could not be found but ".emacs.d/" could use that
+; Else create the user-directory
+(setq user-emacs-directory (locate-user-emacs-file user-emacs-directory ".emacs.d/"))
+
+
+;;;; Finish importing Windows functions now user-emacs-dir has been checked
+(when (string-equal system-type "windows-nt")
+  (add-to-list 'exec-path (concat user-emacs-directory "win")))	      	; locate required windows files(included)
+  (setq pdf-info-epdfinfo-program (concat user-emacs-directory "win/epdfinfo.exe"))
+  (setq TeX-macro-global						; MAY add where you've downloaded Texlive, required for Latex
    '("c:/texlive/2020/texmf-var/tex/" "c:/texlive/texmf-local/tex/" "c:/texlive/texmf-local/bibtex/bst/" "c:/texlive/2020/texmf-dist/tex/" "c:/texlive/2020/texmf-dist/bibtex/bst/"))
-;;; Locate linux functions in windows
-  (add-to-list 'exec-path "~/.emacs.d/win") 				; additional Windows files already installed
-  (setenv "PATH"
+  (load (concat user-emacs-directory "win/win-keys.el") 'noerror)	; Adds some Windows Keybinds(not many yet) MAY remove this
+;;; Locate linux functions in windows 
+  (setenv "PATH"							; MAY, fill these with your own versions
     (concat							       	; Paths for Windows version of linux executables
      "C:/msys64/mingw64/bin" ";"					; if you have it installed it is prefered over cygwin
      "C:/texlive/2020/bin/win32" ";" 					; PDFlatex
@@ -27,17 +40,8 @@
      "C:/cygwin64/bin" ";"
      "C:/Program Files/Git/bin" ";"	 				; Git
      (getenv "PATH")))
-  (load "~/.emacs.d/win/win-keys.el" 'noerror))
 
 
-;;;; Link files in Linux
-(when (string-equal system-type "gnu/linux")
-  (setenv "HOME" "/home/chaos")			       			; this is where '~' refers to
-  (setq user-emacs-directory "~/.emacs.d/")
-    (setq TeX-macro-global "/usr/bin/")
-   )
-  
-
 ;;;; Program specific links 						; SHOULD, set these to the directorys you are using under HOME
 (setq default-directory "~/docs/")					; default when searching
 (setq org-directory "~/docs/")						; where your org-notes are stored
@@ -47,3 +51,5 @@
 (setq org-ref-pdf-directory "~/gdrive/Library/")
 (setq org-ref-default-bibliography "~/docs/writeup/roam.bib") 		; Your main bibleography
 (setq bibtex-completion-bibliography "~/docs/writeup/roam.bib")
+
+
