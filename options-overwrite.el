fda5aaff9ba2d75e17bf2e7c964379cb5dd2a64e
Toby Rowlands <rowlandsToby1@gmail.com>
2021-04-29 03:22:59 +0100

    Reuse paths when setting

--- options-overwrite.el
+++ options-overwrite.el
@@ -1,14 +1,23 @@
 ;;; Program specific paths 						; SHOULD, set these to the directorys you are using under HOME(set in .emacs)
 ; if you use different file structures on each OS you may wish to add OS-specific pathis in the settings below 
-(setq
- default-directory "~/digfog/"						; default when searching
- org-directory "~/digfog/"						; where your org-notes are stored
- org-download-image-dir "../gdrive/Library/img/misc/"			; Where you want images downloaded with org-download stored
- org-roam-directory "~/digfog/roam"		       				; Where you keep your org-roam notes
- org-brain-path "~/digfog/brain"					; Structured org-brain notes
- org-ref-pdf-directory "../gdrive/Library/"
- org-ref-default-bibliography "~/digfog/main.bib" 		; Your main bibleography
- reftex-default-bibliography `("~/digfog/main.bib") 
- bibtex-completion-bibliography `("~/digfog/main.bib")
- bibtex-completion-library-path "../gdrive/Library/"
+
+(let
+  ((working-dir "~/digfog/")
+  (bib-file "main.bib")
+  (library-dir "~/../gdrive/Library/"))
+  (setq
+    default-directory (format "%s" working-dir)						; default when searching
+    org-directory  (format "%s" working-dir)				; where your org-notes are stored
+    org-download-image-dir (format "%simg/misc/" library-dir)		; Where you want images downloaded with org-download stored
+    org-roam-directory (format "%sroam/" working-dir)		       				; Where you keep your org-roam notes
+    org-brain-path (format "%sbrain" working-dir)					; Structured org-brain notes
+    reftex-default-bibliography `(,(format "%s%s" working-dir bib-file))
+    ;backquote (`) is a special form of ' that allows evaluating expressions prefixed with (,)
+    org-ref-bibliography-notes (format "%sroam/notes.org" working-dir)
+    org-ref-default-bibliography `(,(format "%s%s" working-dir bib-file))
+    org-ref-pdf-directory (format "%s" library-dir)
+    bibtex-completion-bibliography (format "%s%s" working-dir bib-file)
+    bibtex-completion-library-path (format "%s" library-dir)
+    bibtex-completion-notes-path (format "%snotes" org-roam-directory)
+  )
 )
